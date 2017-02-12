b3a3a18e765959cb17390a8ac8b5a0a0405eb5d3
Wahib <wahib.tech@gmail.com>
2017-02-12 01:24:30 +0100

    implemented basic ui, arrays and strings

--- app/src/main/java/projects/hobby/urdufontcomparator/fragments/MainFragment.java
+++ app/src/main/java/projects/hobby/urdufontcomparator/fragments/MainFragment.java
@@ -1,20 +1,64 @@
 package projects.hobby.urdufontcomparator.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import projects.hobby.urdufontcomparator.R;
 
 public class MainFragment extends Fragment {
+
+    private Unbinder unBinder;
+
+    @BindView(R.id.spinner_font_names)
+    Spinner spinnerFontNames;
+
+    @BindView(R.id.text_body)
+    TextView textBody;
 
     public static MainFragment newInstance() {
         return new MainFragment();
     }
 
-    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        unBinder = ButterKnife.bind(this, view);
+
+        setupUI();
+    }
+
+    private void setupUI() {
+        spinnerFontNames.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapter, View view, int position, long id) {
+                final String fontName = adapter.getItemAtPosition(position).toString();
+                textBody.setText(fontName);
+            }
+
+            @Override public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    @Override public void onDestroyView() {
+        unBinder.unbind();
+        super.onDestroyView();
     }
 }
