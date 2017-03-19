20dd9ddadb3c3f56532c76e04be6c319fe1c1653
Wahib Ul Haq <wahib.tech@gmail.com>
2017-03-19 12:39:19 +0100

    Home Page Layout Improved (#2)
    * Boundaries added, Layout updated, Colors updated
    
    This is a bit major change how i think we should layout the home page
    
    * reviews addressed

--- app/src/main/java/projects/hobby/urdufontcomparator/utils/UiUtils.java
+++ app/src/main/java/projects/hobby/urdufontcomparator/utils/UiUtils.java
@@ -68,8 +68,9 @@ public class UiUtils {
 
         //Setting textColor of button
         Button posButton = dialog.getButton(DialogInterface.BUTTON_POSITIVE);
-        if(posButton!= null) {
-            posButton.setTextColor(ContextCompat.getColor(context, R.color.teal_500));
+        if (posButton!= null) {
+            posButton.setBackgroundColor(ContextCompat.getColor(context, R.color.colorAccent));
+            posButton.setTextColor(ContextCompat.getColor(context, R.color.white));
         }
     }
 
