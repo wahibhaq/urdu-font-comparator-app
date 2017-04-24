96830806b3202b3c02d2eb46f3e85117205cdd3b
ZeeshanShabbir <zeeshan_shabbir79@gmail.com>
2017-04-24 10:20:15 +0500

    implemented feedback

--- app/src/main/java/projects/hobby/urdufontcomparator/activities/BaseActivity.java
+++ app/src/main/java/projects/hobby/urdufontcomparator/activities/BaseActivity.java
@@ -1,12 +1,20 @@
 package projects.hobby.urdufontcomparator.activities;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import projects.hobby.urdufontcomparator.R;
@@ -18,12 +26,13 @@ public abstract class BaseActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     protected Toolbar toolbar;
 
-    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setupToolbar();
-        if(savedInstanceState == null) {
+        if (savedInstanceState == null) {
             showDefaultFragment();
         }
     }
@@ -36,10 +45,12 @@ public abstract class BaseActivity extends AppCompatActivity {
     //defined by the child activities
     protected abstract void showDefaultFragment();
 
-    @Override public boolean onCreateOptionsMenu(Menu menu) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
         toolbar.inflateMenu(R.menu.actionbar_menu);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override public boolean onMenuItemClick(MenuItem item) {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
                 return onOptionsItemSelected(item);
             }
         });
@@ -62,6 +73,29 @@ public abstract class BaseActivity extends AppCompatActivity {
                         urduTextSource.prepareDevsInfoDialogText());
                 return true;
 
+            case R.id.action_email:
+                String[] reception = {getString(R.string.dev_email)};
+                Intent email = new Intent(Intent.ACTION_SEND, Uri.parse(getString(R.string.mail_to)));
+                email.setType(getString(R.string.intent_type));
+                email.putExtra(Intent.EXTRA_EMAIL, reception);
+                try {
+                    startActivity(Intent.createChooser(email, getString(R.string.send_email)));
+                } catch (Exception ex) {
+                    Toast.makeText(this, R.string.email_app_isnt_installed_message, Toast.LENGTH_SHORT).show();
+                }
+                return true;
+
+            case R.id.action_tweet:
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.dev_twitter_handle));
+                intent.setType(getString(R.string.intent_type));
+                if (isTwitterInstalled(intent)) {
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, R.string.twitter_app_isnt_installed_message, Toast.LENGTH_SHORT).show();
+                }
+                return true;
+
             default:
                 // If we got here, the user's action was not recognized.
                 // Invoke the superclass to handle it.
@@ -70,4 +104,20 @@ public abstract class BaseActivity extends AppCompatActivity {
         }
     }
 
+    public boolean isTwitterInstalled(Intent intent) {
+        PackageManager packManager = getPackageManager();
+        List<ResolveInfo> resolvedInfoList = packManager.queryIntentActivities(intent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        boolean resolved = false;
+        for (ResolveInfo resolveInfo : resolvedInfoList) {
+            if (resolveInfo.activityInfo.packageName.startsWith(getString(R.string.twitter_identifier))) {
+                intent.setClassName(
+                        resolveInfo.activityInfo.packageName,
+                        resolveInfo.activityInfo.name);
+                resolved = true;
+                break;
+            }
+        }
+        return resolved;
+    }
 }
