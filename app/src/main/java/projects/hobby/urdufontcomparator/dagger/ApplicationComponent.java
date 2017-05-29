31a8aa86f42418e489553cbe18cf47f408a8411d
Wahib-Ul-Haq <wahib.tech@gmail.com>
2017-05-30 00:33:14 +0200

    UrduTextSource now only used through injection

--- app/src/main/java/projects/hobby/urdufontcomparator/dagger/ApplicationComponent.java
+++ app/src/main/java/projects/hobby/urdufontcomparator/dagger/ApplicationComponent.java
@@ -3,10 +3,13 @@ package projects.hobby.urdufontcomparator.dagger;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import projects.hobby.urdufontcomparator.activities.BaseActivity;
 
 @Singleton
 @Component(modules = {AppModule.class, RetroApiModule.class})
 public interface ApplicationComponent {
+
+    void inject(BaseActivity baseActivity);
 
     //Subcomponent
     MainMvpComponent mvpComponent(MainMvpModule mainMvpModule);
