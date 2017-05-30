eab641f89fac0e4183f144e211df79a6d44bb6ab
Wahib-Ul-Haq <wahib.tech@gmail.com>
2017-05-30 14:47:30 +0200

    missed AppCompatActivity import during rebasing

--- app/src/main/java/projects/hobby/urdufontcomparator/fragments/MainFragment.java
+++ app/src/main/java/projects/hobby/urdufontcomparator/fragments/MainFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
